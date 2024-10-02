8ba16f9f3aafa785cd1b43c1d2ca20b9fbbf13e8
VRosa-jpg <vixkirosa@gmail.com>
2024-10-02 11:15:33 -0400

    Update: main2.js logic

--- main2.js
+++ main2.js
@@ -3,22 +3,32 @@
 
 
 // If it is a prime number
+// Does not need a number because it is saying whether it is prime number
+// Next time make a function like this to just verify whether it is true or not
+// because you can use another function to output what is needed 
 function isPrime(num) {
+    if (num < 2) return false;
     for (let i = 2; i < num; i++){
         if (num%i === 0){
-            console.log(num)
-            break
+            return false
+        } else {
+            return true
         }
     }
 }
 
-// The next prime number
-// function nextPrime(n) {
-//     for (let i = 1; n > i; i++){
-//         if ( i == isPrime(num))
-//             break
-//     }
-// }
+function nextPrime(num) {
+    let next = num + 1;
+    while(true) {
+        if (isPrime(next)) {
+            return next;
+        }
+        next++;
+    }
+}
+
+console.log(nextPrime(21))
 
 
-// nextPrime(4)
+
+
